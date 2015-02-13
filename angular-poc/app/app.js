1f2e56778d4405138b79940703979f89917f5a4a
James Parker <james.parker@aciworldwide.com>
2015-02-13 18:00:44 -0500

    introducing per-widget angular controllers

--- angular-poc/app/app.js
+++ angular-poc/app/app.js
@@ -1,8 +1,21 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('GreetingController', function($scope, $http) {
-	$http.get("http://www.w3schools.com/website/Customers_JSON.php")
-		.success(function(response) {
-			$scope.names = response;
-		});
+myApp.controller('dataController', function($scope, $http) {
+  $http.get("http://www.w3schools.com/website/Customers_JSON.php")
+    .success(function(response) {
+      $scope.names = response;
+    });
+});
+
+myApp.controller('dataController2', function($scope) {
+  $scope.names = [{
+    Name: 'Jani',
+    Country: 'Norway'
+  }, {
+    Name: 'Hege',
+    Country: 'Sweden'
+  }, {
+    Name: 'Kai',
+    Country: 'Denmark'
+  }];
 });
